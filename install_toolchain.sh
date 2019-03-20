15b5d5c94fcb4a46cc6871a18b6db9087cbbaf79
Vahempio <vahempio@gmail.com>
2019-03-20 20:08:36 +0100

    arm-none-eabi-gdb: upgrade to 8-2018-q4

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2"
-TOOLCHAIN_FILE="gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2"
-TOOLCHAIN_MD5="d3b00ae09e847747ef11316a8b04989a"
+TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2"
+TOOLCHAIN_FILE="gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2"
+TOOLCHAIN_MD5="f55f90d483ddb3bcf4dae5882c2094cd"
 
 wget -nv -O $TOOLCHAIN_FILE $TOOLCHAIN_URL
 
@@ -14,6 +14,6 @@ then
 fi
 
 tar -xf $TOOLCHAIN_FILE -C /opt/
-ln -s /opt/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-gdb /opt/arm-gdb
+ln -s /opt/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-gdb /opt/arm-gdb
 
 rm $TOOLCHAIN_FILE
